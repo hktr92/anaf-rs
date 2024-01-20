ff1f69dbbfa818f8f2a8c97cbc1489cc951d6f12
Petru Szemereczki <petru.szemereczki@proton.me>
2024-01-20 15:13:05 +0200

    misc(balance-api): rename balance.rs to balance_wrapper.rs to prevent module ambiguity

--- src/apis/balance/mod.rs
+++ src/apis/balance/mod.rs
@@ -1,11 +1,11 @@
 mod api;
-mod balance;
+mod balance_wrapper;
 mod request;
 mod response;
 mod version;
 
 pub use api::*;
-pub use balance::*;
+pub use balance_wrapper::*;
 pub use request::*;
 pub use response::*;
 pub use version::*;
